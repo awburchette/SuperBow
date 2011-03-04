11fbf525f563dbdfb6ca30fb050c1cef1cca2e51
Anthony Burchette <awburchette@gmail.com>
2011-03-03 21:13:10 -0500

    Adding project files

---
+++ src/bitswitch/SuperBow/SuperBowBlockListener.java
@@ -0,0 +1,22 @@
+package bitswitch.SuperBow;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPhysicsEvent;
+
+/**
+ * SuperBow block listener
+ * @author bitswitch
+ */
+public class SuperBowBlockListener extends BlockListener {
+    private final SuperBow plugin;
+
+    public SuperBowBlockListener(final SuperBow plugin) {
+        this.plugin = plugin;
+    }
+
+    //put all Block related code here
+}
---
+++ src/bitswitch/SuperBow/SuperBowPlayerListener.java
@@ -0,0 +1,23 @@
+package bitswitch.SuperBow;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author bitswitch
+ */
+public class SuperBowPlayerListener extends PlayerListener {
+    private final SuperBow plugin;
+
+    public SuperBowPlayerListener(SuperBow instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code here
+}
+
